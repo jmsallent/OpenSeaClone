7ef6539c33f2a2bed3129ec6415cb6be25c3e10a
Jose Miguel <jmsallent@gmail.com>
2023-01-12 16:26:14 +0100

    ThirdWeb Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+const desiredChainId = ChainId.Mumbai;
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider desiredChainId={desiredChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
